c964e3cefdb43b704a8be76519709c0ca59fdf52
Mukhriddin <ostonovmb@gmail.com>
2021-09-21 11:58:00 +0500

    prettier configured

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //create a react component
-const App = () =>{
-    return <div>Hi there!</div>;
+const App = () => {
+  return <div>Hi there!</div>;
 };
 
 //take the react component and show it on the screen
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
