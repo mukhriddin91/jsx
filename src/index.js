325aa713cb23b915a4f5066dbd64bdfe099890cd
Mukhriddin <ostonovmb@gmail.com>
2021-09-22 11:02:57 +0500

    Section 2: Building Content with JSX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import ReactDOM from "react-dom";
 
 //create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+
+  const buttonText = { text: 'Click Me!' };
+  const labelText = 'Enter your name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 //take the react component and show it on the screen
