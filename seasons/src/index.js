09b8b61652376f19a6861b2a5ee3e423ef50a132
Mukhriddin <ostonovmb@gmail.com>
2021-09-27 13:52:43 +0500

    Section 6: Understanding Lifecycle Methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,34 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: '' };
 
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
-    }
+    };
 
-    render() {
+    renderContent(){
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat}/>;
         }
 
-        return <div>Loading!</div>;
-    }
+        return <Spinner/>;
+    };
+
+    render() {
+      return (
+        <div className="border red">
+            {this.renderContent()}
+        </div>
+      );
+    };
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
